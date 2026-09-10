29106423b012add3adfc2789b9236e661d8c0e8e
Ilya Bikmeev <i@ibikmeev.ru>
2026-09-11 00:31:39 +0500

    Read temperature ds18b20 implementation

--- internal/sensor/ds18b20.go
+++ internal/sensor/ds18b20.go
@@ -1,10 +1,78 @@
 package sensor
 
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type DS18B20 struct {
-	id string
+	id          string
+	w1SlavePath string
 }
 
-// TODO реализовать чтение температуры с датчика DS18B20
+func NewDS18B20(id string, w1SlavePath string) *DS18B20 {
+	return &DS18B20{
+		id:          id,
+		w1SlavePath: w1SlavePath,
+	}
+}
+
+func (ds *DS18B20) ID() string {
+	return ds.id
+}
+
+// TODO возвращать реальное имя
+func (ds *DS18B20) Name() string {
+	return "DS18B20"
+}
+
+func (ds *DS18B20) ReadTemperature(ctx context.Context) (float64, error) {
+	select {
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	default:
+	}
+
+	data, err := os.ReadFile(ds.w1SlavePath)
+	if err != nil {
+		return 0, fmt.Errorf("read sensor %s: %w", ds.id, err)
+	}
+
+	content := string(data)
+
+	if !strings.Contains(content, "YES") {
+		return 0, fmt.Errorf("sensor %s: CRC check failed", ds.id)
+	}
+
+	position := strings.LastIndex(content, "t=")
+
+	if position == -1 {
+		return 0, fmt.Errorf("sensor %s: temperature data not found", ds.id)
+	}
+
+	rawTemperature := strings.TrimSpace(
+		content[position+2:],
+	)
+
+	milliDegrees, err := strconv.ParseInt(
+		rawTemperature,
+		10,
+		64,
+	)
+
+	if err != nil {
+		return 0, fmt.Errorf(
+			"parse sensor %s temperature: %w",
+			ds.id,
+			err,
+		)
+	}
+	return float64(milliDegrees) / 1000, nil
+}
+
 func DiscoverDS18B20Sensors(devicesPath string) ([]TemperatureSensor, error) {
 	//TODO реализовать обнаружение датчиков DS18B20
 	return nil, nil
