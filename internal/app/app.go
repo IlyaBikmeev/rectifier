44a97e8e0208f0a7e16abca6594f835b65db6238
Ilya Bikmeev <i@ibikmeev.ru>
2026-09-10 22:25:33 +0500

    Implemented graceful shutdown

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,9 +1,15 @@
 package app
 
 import (
+	"context"
 	_ "embed"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 //go:embed templates/index.html
@@ -11,18 +17,54 @@ var indexHTML string
 
 func Run() {
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("GET /", handleIndex)
 
-	addr := ":8080"
-
-	fmt.Printf("Server started on %s\n", addr)
-
-	err := http.ListenAndServe(addr, mux)
-
-	if err != nil {
-		fmt.Printf("Error starting server: %v\n", err)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
 	}
+
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		fmt.Printf("Server started on %s\n", server.Addr)
+
+		serverErrors <- server.ListenAndServe()
+
+	}()
+
+	shutdownSignal := make(chan os.Signal, 1)
+	signal.Notify(
+		shutdownSignal,
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+
+	select {
+	case sig := <-shutdownSignal:
+		fmt.Printf("Received signal: %s\n", sig)
+
+	case err := <-serverErrors:
+		if !errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("Server error: %v\n", err)
+		}
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		5*time.Second,
+	)
+	defer cancel()
+
+	fmt.Println("Shutting down server...")
+
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Printf("Error shutting down server: %v\n", err)
+		return
+	}
+
+	fmt.Println("Server stopped")
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
